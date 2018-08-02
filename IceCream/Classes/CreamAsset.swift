f6031751251e5a36d1e9a69ec6151d0e13ec89ad
segunleedev@gmail.com <segunlee@SegunLeeui-MacBook-Pro.local>
2018-08-02 17:49:06 +0900

    Change createAssetDefaultUrl SearchPathDirectory

--- IceCream/Classes/CreamAsset.swift
+++ IceCream/Classes/CreamAsset.swift
@@ -105,7 +105,7 @@ extension CreamAsset {
     /// The default path for the storing of CreamAsset. That is:
     /// xxx/Document/CreamAsset/
     public static func creamAssetDefaultURL() -> URL {
-        let documentDir = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+        let documentDir = try! FileManager.default.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
         let commonAssetPath = documentDir.appendingPathComponent(className())
         if !FileManager.default.fileExists(atPath: commonAssetPath.path) {
             do {
